1336ed8bef014a3b8400d8b94841c7e241d72839
Bronsen Ivulich <bronsenivulich.j@gmail.com>
2022-08-31 18:21:51 +0800

    test 2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>jfwkjwejkcnewckjwenckj</Text>
+      <Text>jfwkjwejkcnerwgfregregerewckjwenckj</Text>
       <StatusBar style="auto" />
     </View>
   );
