f7ec3e25f9c5576ffb41160e6e6729ac540ddfdf
Bronsen Ivulich <bronsenivulich.j@gmail.com>
2022-09-03 01:01:13 +0800

    add navigation and js templates

--- navigation/screens/Announcements.js
+++ navigation/screens/Announcements.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function AnnouncementsScreen() {
