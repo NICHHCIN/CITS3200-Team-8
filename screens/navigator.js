e6565444c2bba3157abb3d38c859bb9e4081a09d
Jacob12245 <22891566@student.uwa.edu.au>
2022-10-11 18:28:05 +0800

    removed redundant failed implementation of redux

--- screens/navigator.js
+++ screens/navigator.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
