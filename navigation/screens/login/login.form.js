3bb0df7aef72849dfe2222f634ee95184f0c6e5a
Jacob12245 <22891566@student.uwa.edu.au>
2022-10-12 18:25:58 +0800

    remove redundant navigation folder

--- navigation/screens/login/login.form.js
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup';
-
-export const loginform = yup.object().shape({
-	email: yup.string().required("Required paramter").email("Invalid email"),
-	password: yup.string().required("Required paramter")
-})
--- screens/AboutPage.js
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { Button, View, Alert, Text } from 'react-native';
-const AboutPage = () => {
-   return <Text>You have reached inside About Page!</Text>;
-};
-export default AboutPage;
